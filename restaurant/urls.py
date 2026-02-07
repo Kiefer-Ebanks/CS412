1f76c6f9dc5585376e1a32ed8121348015258f67
Kiefer Ebanks <kebanks@bu.edu>
2026-02-06 19:08:30 -0500

    finished the restaurant app

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -10,4 +10,6 @@ from . import views
  
 urlpatterns = [ 
     path(r'', views.main, name="main"),
+    path(r'order/', views.order, name="order"),
+    path(r'confirmation/', views.confirmation, name="confirmation"),
 ]
