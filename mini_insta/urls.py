d1aa4b4733357b8257f1f1be9e2200f0de52e560
Kiefer Ebanks <kieferebanks@gmail.com>
2026-02-13 16:20:48 -0500

    finished my mini_insta hw1

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -5,8 +5,10 @@
 
 
 from django.urls import path
-from .views import ProfileListView
+from .views import ProfileListView, ProfileDetailView
 
+# Creating the url paths for the mini instagram app
 urlpatterns = [
     path(r'', ProfileListView.as_view(), name='show_all_profiles'),
+    path(r'profile/<int:pk>/', ProfileDetailView.as_view(), name='show_profile'),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView # Importing the ListView class
+from django.views.generic import ListView, DetailView # Importing the ListView and DetailView classes
 from .models import Profile # Importing the Profile model from the models.py file
 # Create your views here.
 
@@ -9,4 +9,12 @@ class ProfileListView(ListView):
     ''' A view to display a list of all profiles '''
     model = Profile
     template_name = 'mini_insta/show_all_profiles.html'
-    context_object_name = 'profiles'
+    context_object_name = 'profiles' # using plural variable name for the profiles list
+
+
+class ProfileDetailView(DetailView):
+    ''' A view to display a single profile '''
+
+    model = Profile
+    template_name = 'mini_insta/show_profile.html'
+    context_object_name = 'profile' # using singular variable name for the profile object
