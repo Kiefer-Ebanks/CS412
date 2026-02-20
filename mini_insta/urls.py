2ebac55ee5c5411e9c67d472d2cae61926525965
Kiefer Ebanks <kieferebanks@gmail.com>
2026-02-20 16:05:37 -0500

    Finished Task 1

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -5,10 +5,11 @@
 
 
 from django.urls import path
-from .views import ProfileListView, ProfileDetailView
+from .views import ProfileListView, ProfileDetailView, PostDetailView
 
 # Creating the url paths for the mini instagram app
 urlpatterns = [
     path(r'', ProfileListView.as_view(), name='show_all_profiles'),
     path(r'profile/<int:pk>/', ProfileDetailView.as_view(), name='show_profile'),
+    path(r'post/<int:pk>/', PostDetailView.as_view(), name='show_post'),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView # Importing the ListView and DetailView classes
-from .models import Profile # Importing the Profile model from the models.py file
+from .models import Profile, Post # Importing the Profile model from the models.py file
 # Create your views here.
 
 
@@ -18,3 +18,11 @@ class ProfileDetailView(DetailView):
     model = Profile
     template_name = 'mini_insta/show_profile.html'
     context_object_name = 'profile' # using singular variable name for the profile object
+
+
+class PostDetailView(DetailView):
+    ''' A view to display a single post '''
+
+    model = Post
+    template_name = 'mini_insta/show_post.html'
+    context_object_name = 'post' # using singular variable name for the post object
